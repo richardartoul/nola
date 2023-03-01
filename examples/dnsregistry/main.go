32d908ea02c6728cf706cce351ac50fd771d1b3c
Richard Artoul <richardartoul@gmail.com>
2023-03-01 00:22:03 -0600

    fix example

--- examples/dnsregistry/main.go
+++ examples/dnsregistry/main.go
@@ -54,7 +54,7 @@ func main() {
 
 			ctx, cc := context.WithTimeout(context.Background(), 5*time.Second)
 			actorID := fmt.Sprintf("actor-%d", i%10)
-			resp, err := env.InvokeActor(
+			resp, err := env.InvokeActorBytes(
 				ctx,
 				"example", actorID, "test-module", "inc", nil,
 				types.CreateIfNotExist{})
